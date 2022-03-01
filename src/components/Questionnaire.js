b59341b41b1f788a4a64925e845ad2a96d842f81
natia-malazonia <malazonianatia@gmail.com>
2022-03-01 14:13:28 +0400

    Added fonts, Created main context of Questionnaire page

---
+++ src/components/Questionnaire.js
@@ -0,0 +1,9 @@
+function Questionnaire() {
+    return <div>
+        <h1>Welcome Rocketeer!</h1>
+        <button>Start Questionnaire</button>
+        <span>Submitted applications</span>
+    </div>
+}
+
+export default Questionnaire
